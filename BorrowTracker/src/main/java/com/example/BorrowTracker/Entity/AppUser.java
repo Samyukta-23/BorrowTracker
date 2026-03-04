3b87aec56a504c2060829ed03cf3c58634c8c742
Samyukta A <asamyukta2006@gmail.com>
2026-03-04 12:22:45 +0530

    Add files via upload

---
+++ BorrowTracker/src/main/java/com/example/BorrowTracker/Entity/AppUser.java
@@ -0,0 +1,21 @@
+package com.example.BorrowTracker.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+}
